42fdc1e944222176db5590d4834eccb91939ea22
Max Luan <max_luan@foxmail.com>
2023-04-06 00:40:22 +0800

    .

--- untrack.sh
+++ untrack.sh
@@ -2,5 +2,5 @@ git update-index --assume-unchanged temp/clash.yaml
 git update-index --assume-unchanged temp/config.yaml 
 git update-index --assume-unchanged temp/proxy.txt  
 git update-index --assume-unchanged conf/config.yaml 
-git update-index --assume-unchanged conf/cache.db 
 git update-index --assume-unchanged logs/clash.log
+git update-index --assume-unchanged .env
