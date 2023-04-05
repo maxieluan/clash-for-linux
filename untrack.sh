98e33fb4236e627ba4133117864ef0ce76bf3a71
Max Luan <max_luan@foxmail.com>
2023-04-06 00:33:28 +0800

    some tweaks

--- untrack.sh
+++ untrack.sh
@@ -3,3 +3,4 @@ git update-index --assume-unchanged temp/config.yaml
 git update-index --assume-unchanged temp/proxy.txt  
 git update-index --assume-unchanged conf/config.yaml 
 git update-index --assume-unchanged conf/cache.db 
+git update-index --assume-unchanged logs/clash.log
